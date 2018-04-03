6aea6f0e1ff7b67d62980f9be68fa92ea48235dc
Maksim <mak.kooz@gmail.com>
2018-04-03 13:45:42 +0300

    Added options (transform, each, model)

--- src/fromInput.js
+++ src/fromInput.js
@@ -1,4 +1,4 @@
-const fromInput = function (inputAttr) {
+const fromInput = function (inputAttr, options) {
   return function (target, propertyKey, descriptor) {
     target.__mapper = target.__mapper || {
       properties: []
@@ -17,7 +17,21 @@ const fromInput = function (inputAttr) {
           if (index < parts.length - 1) {
             inputContext = inputContext[part]
           } else {
-            context[propertyKey] = inputContext[part]
+            const value = inputContext[part]
+            if (options && options.each && Array.isArray(value)) {
+              context[propertyKey] = value.map(x => options.each(x))
+            } else if (options && options.transform) {
+              context[propertyKey] = options.transform(value)
+            } else if (options && options.model) {
+              const Model = options.model
+              if (Array.isArray(value)) {
+                context[propertyKey] = value.map(x => new Model(x))
+              } else {
+                context[propertyKey] = new Model(value)
+              }
+            } else {
+              context[propertyKey] = value
+            }
           }
         } else {
           inputContext = null
